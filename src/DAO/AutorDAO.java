1b9bd60642e7f940ce57fa6d622f87b5df9cb924
Matheus- <mats-@PC-Matheus>
2018-08-02 18:03:50 -0300

    Adicionando codigo do autorDAO e GeneroDAO

--- src/DAO/AutorDAO.java
+++ src/DAO/AutorDAO.java
@@ -27,5 +27,56 @@ public class AutorDAO {
         }
 
     }
+    
+    public ArrayList<Autor> BuscarAutor() throws SQLException {
+
+        //Buscando uma conexão com o Banco de Dados
+        Connection con = ConectaBanco.getConexao();
+        /*Criando obj. capaz de executar instruções
+         SQL no banco de dados*/
+        Statement stat = con.createStatement();
+
+        try {
+            //Criando variável sql vazia
+            String sql;
+
+            //Montando o sql
+            sql = "select * from autor";
+
+            /* Executando o SQL  e armazenando
+             o ResultSet em um objeto do tipo
+             ResultSet chamado rs */
+            ResultSet rs = stat.executeQuery(sql);
+
+            /* Criando ArrayList para armazenar 
+             objetos do tipo produto */
+            ArrayList<Autor> autores = new ArrayList<>();
+
+            /* Enquanto houver uma próxima linha no 
+             banco de dados o while roda */
+            while (rs.next()) {
+                //Criando um novo obj. ProdutoVO
+                Autor autor = new Autor();
+
+                /* Mapeando a tabela do banco para objeto
+                 chamado pVO */
+                autor.setCodigo(rs.getInt("codigo"));
+                autor.setNome(rs.getString("nome"));
+
+
+                /* Inserindo o objeto pVO no ArrayList */
+                autores.add(autor);
+
+            }//Fecha while
+
+            //Retornando o ArrayList com todos objetos
+            return autores;
+
+        } catch (SQLException se) {
+            throw new SQLException("Erro ao buscar dados do Banco! " + se.getMessage());
+        } finally {
+
+        }//fecha finally
+    }
 
 }
--- src/DAO/GeneroDAO.java
+++ src/DAO/GeneroDAO.java
@@ -29,4 +29,55 @@ public class GeneroDAO {
 
     }
 
+    public ArrayList<Genero> BuscarGenero() throws SQLException {
+
+        //Buscando uma conexão com o Banco de Dados
+        Connection con = ConectaBanco.getConexao();
+        /*Criando obj. capaz de executar instruções
+         SQL no banco de dados*/
+        Statement stat = con.createStatement();
+
+        try {
+            //Criando variável sql vazia
+            String sql;
+
+            //Montando o sql
+            sql = "select * from genero";
+
+            /* Executando o SQL  e armazenando
+             o ResultSet em um objeto do tipo
+             ResultSet chamado rs */
+            ResultSet rs = stat.executeQuery(sql);
+
+            /* Criando ArrayList para armazenar 
+             objetos do tipo produto */
+            ArrayList<Genero> generos = new ArrayList<>();
+
+            /* Enquanto houver uma próxima linha no 
+             banco de dados o while roda */
+            while (rs.next()) {
+                //Criando um novo obj. ProdutoVO
+                Genero genero = new Genero();
+
+                /* Mapeando a tabela do banco para objeto
+                 chamado pVO */
+                genero.setCodigo(rs.getInt("codigo"));
+                genero.setTipo(rs.getString("tipo"));
+
+
+                /* Inserindo o objeto pVO no ArrayList */
+                generos.add(genero);
+
+            }//Fecha while
+
+            //Retornando o ArrayList com todos objetos
+            return generos;
+
+        } catch (SQLException se) {
+            throw new SQLException("Erro ao buscar dados do Banco! " + se.getMessage());
+        } finally {
+
+        }//fecha finally
+    }
+
 }
