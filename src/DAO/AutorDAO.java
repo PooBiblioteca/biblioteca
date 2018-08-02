b70ea658e1e81c23ea6bbb37f4af275fcec4bdc3
Matheus- <mats-@PC-Matheus>
2018-08-02 17:50:55 -0300

    Adicionando tudo de novo

--- src/DAO/AutorDAO.java
+++ src/DAO/AutorDAO.java
@@ -5,21 +5,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import Entidade.Genero;
-import DAO.ConectaBanco;
+import Entidade.Autor;
 import java.sql.PreparedStatement;
 
-public class GeneroDAO {
+public class AutorDAO {
 
-    public void Salvar(Genero genero) {
-        String sql = "INSERT INTO genero(codigo,tipo) VALUES(?,?)";
+    public void Salvar(Autor autor) {
+        String sql = "INSERT INTO autor(codigo,nome) VALUES(?,?)";
         try {
             Connection con = ConectaBanco.getConexao();
             /*Criando obj. capaz de executar instruções
          SQL no banco de dados*/
             PreparedStatement stat = con.prepareStatement(sql);
-            stat.setInt(1, genero.getCodigo());
-            stat.setString(2, genero.getTipo());
+            stat.setInt(1, autor.getCodigo());
+            stat.setString(2, autor.getNome());
 
             stat.execute();
             stat.close();
